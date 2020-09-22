ba59f3ef78ec384db13a8c0ef12b65025c6db306
Pierce O'Neill <pierceoneillky@gmail.com>
2020-09-22 12:06:45 +0100

    Adding Auth route and verified working with postman

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
